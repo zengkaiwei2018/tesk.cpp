2c6ad0fbf5a494832bf7932286f8d82bc9cfee48
zengkaiwei2018 <1912207369@qq,com>
2019-06-20 11:56:11 +0800

    选手信息续代码

--- tesk.cpp/main.cpp
+++ tesk.cpp/main.cpp
@@ -6,10 +6,49 @@
 //  Copyright © 2019 s20181105306. All rights reserved.
 //
 
+//#include <iostream>
+
+//int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+   // return 0;
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//int main(int argc, const char * argv[]) {
+// insert code here...
+//std::cout << "Hello, World!\n";
+//eturn 0;
+//}
+#include <fstream>
+#include"string.h"
+#include"stdlib.h"
+using namespace std;
+struct student
+int main(){
+    ofstream count(/Users/s20181105306/Desktop/test.cpp);
+    ifstream int(/Users/s20181105306/Desktop/选手信息的副本.txt)
+    {
+        if(int.is_open()){
+            char num[10];
+            char sex[5];
+            char age[2];
+            int remark;
+            struct student*prior;
+            struct student*next;}l;
+        struct student*start*,*last;
+        void enter(),list(),save(),
+        load(),delet_();
+        void(),delete_();
+        void store(struct student*g);
+        meun,score();
+        
+        
+            
+            
+        
+        
+        couut.close();
+    }
     return 0;
 }
+
