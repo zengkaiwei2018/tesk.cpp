82296d3c99aa820505d18b25a7143b12c8871203
zengkaiwei2018 <51750122+zengkaiwei2018@users.noreply.github.com>
2019-06-25 23:05:14 +0800

    Add files via upload

---
+++ tesk.cpp/未命名1.cpp
@@ -0,0 +1,64 @@
+//#include <iostream>
+	
+	int main(int argc, const char * argv[]) {
+	//int main(int argc, const char * argv[]) {
+	    // insert code here...
+	    std::cout << "Hello, World!\n";
+	    //std::cout << "Hello, World!\n";
+	   // return 0;
+	#include <iostream>
+	
+	//int main(int argc, const char * argv[]) {
+	// insert code here...
+	//std::cout << "Hello, World!\n";
+	//eturn 0;
+	//}
+	#include<iostream>
+	#include<cstring>
+	#include<fstream>
+	#include<stdlib.h>
+	#include<iomanip>
+	#include"string.h"
+	#include"stdlib.h"
+	using namespace std;
+	struct A
+	{
+		string school;
+		string number;
+		string name;
+		string core;
+		float avescore;
+	}
+	student[20];
+	struct B;
+	{
+		string name;
+		float f_point[20];
+	}judge[7];
+	int main();
+	{
+		static int add=0;
+		string point;
+		ifstream fp1("C:\\Users\\开伟\Desktop\\文件2.txt");
+	}
+	static int add=0;
+	do{
+		getline(fp1,student[add].school,' ');
+		getline(fp1,student[add].number,' ');
+		getline(fp1,student[add].name,'\n');add++;
+	}while(!fp1.eof());
+	fp1.close();
+	ifstream fp1("C:\\Users\\曽开伟\\Desktop\\文件1.txt");
+	do{
+		getline(fp2,jude[add2].name,' ');
+		int j=0;
+		peo_num=add;
+		{
+			for(;peo_num=add>1;peo_num--)
+			{
+				getline(fp2,point,' ');
+				jude[add2].f_point[j]=atoi(point.a_str());j++;
+			}
+		}
+		
+	}while(add2!=7);
